ce5a8d020ec162541f1ae2646cef84805d82407d
SuportCoop <s18023647@gmail.com>
2026-02-04 15:52:18 -0300

    Fazendo o Git respeitar o .gitignore

--- login/views.py
+++ login/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from login.forms import AcessoForm
 from login.models import Acesso
+import random
 
 def tela_login(request):
     if request.method == 'POST':
@@ -12,7 +13,7 @@ def tela_login(request):
                 nome=form.cleaned_data['nome'],
                 matricula=form.cleaned_data['matricula']
             )
-            return redirect('https://www.atesa.com.br')
+            return redirect('http://suportecoop.pythonanywhere.com/q/ago-2025-atesa')
     else:
         form = AcessoForm()
     
@@ -21,4 +22,7 @@ def tela_login(request):
 @login_required(login_url='/admin/')
 def lista_acessos(request):
     acessos = Acesso.objects.all().order_by('-data_acesso')
+    nomes = list(acessos.values_list('nome', flat=True))
+    numero = random.choice(list(nomes))
+    print(numero)
     return render(request, 'lista.html', {'acessos': acessos})
