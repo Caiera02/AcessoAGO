ec121c93412265169beed18007684d03859c420b
SuportCoop <s18023647@gmail.com>
2026-02-19 13:39:03 -0300

    criando a tela da delta

--- login/views.py
+++ login/views.py
@@ -17,7 +17,7 @@ def tela_login(request):
     else:
         form = AcessoForm()
     
-    return render(request, 'atesa.html', {'form': form})
+    return render(request, 'deltacooper.html', {'form': form})
 
 @login_required(login_url='/admin/')
 def lista_acessos(request):
